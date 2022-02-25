d177a131c6eff94f5734b23b17ad867d65cf14e8
Optimus-ged <gedeonyalakuhanda@gmail.com>
2022-02-25 14:37:39 +0200

    Understand use of Rows and Columns

--- app/src/main/java/com/example/compose_trainings/MainActivity.kt
+++ app/src/main/java/com/example/compose_trainings/MainActivity.kt
@@ -11,41 +11,50 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_trainings.ui.theme.Compose_trainingsTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                CustomItem(weight = 2f)
+                CustomItem(weight = 6f, color = Color.Yellow)
+            }
         }
     }
 }
 
+@Composable
+fun RowScope.CustomItem(weight: Float, color: Color = Color.Red) {
+    Surface(
+        modifier = Modifier
+            .height(50.dp)
+            .weight(weight),
+        color = color
+    ) {}
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Transparent)
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Surface(
-            modifier = Modifier
-                .height(100.dp)
-                .width(100.dp)
-                .background(Color.Cyan)
-        ) {
-                Column(
-
-                ) {
-                    Text(text = "Optimus gedeon yala")
-                }
-        }
+        CustomItem(weight = 2f)
+        CustomItem(weight = 6f, color = Color.Yellow)
     }
 }
